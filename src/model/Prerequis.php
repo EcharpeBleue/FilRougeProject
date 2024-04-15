de07627c3e5c07db7a41bdd7c2393bc383113f67
Yang Fu <yang.fu3785@gmail.com>
2024-04-15 16:10:52 +0200

    red

--- src/model/Prerequis.php
+++ src/model/Prerequis.php
@@ -48,10 +48,10 @@ class Prerequis
 
 
     
-    public static function update(Prerequis $niveauMin)
+    public static function update(Prerequis $Prerequis)
 {
     $statement = Database::getInstance()->getConnexion()->prepare('UPDATE `PREREQUIS` set equipement=:equipement, niveauMin=:niveaumin, niveauMax=:niveauMax WHERE id =:id');
-    $statement->execute(['equipement'=>$personnage->getEquipement() ,'niveauMin'=>$niveauMin->getNiveauMin(),'niveauMax'=>$personnage->getNiveauMax()]);
+    $statement->execute(['equipement'=>$Prerequis->getEquipement() ,'niveauMin'=>$Prerequis->getNiveauMin(),'niveauMax'=>$Prerequis->getNiveauMax()]);
 }
 
   public static function read(int $id): ?Prerequis
@@ -60,7 +60,7 @@ class Prerequis
       $statement->execute(['id' => $id]);
   
       if ($row = $statement->fetch()) {
-          $prerequis = new Prerequis(id: $row['id']);
+          $prerequis = new Prerequis(id: $row['id'], equipement: $row['equipement'], niveauMin: $row['niveauMin'],niveauMax:$row["niveauMax"]);
           return $prerequis;
       }
   
@@ -70,7 +70,7 @@ class Prerequis
   public static function create (Evenement $evenement,Prerequis $prerequis)
   {
       $statement=Database::getInstance()->getConnexion()->prepare("INSERT INTO `PREREQUIS` (id,equipement,niveauMin,niveauMax) values (:id,:equipement,:niveauMin,:niveauMax);");
-      $statement->execute(['id'=>$Prerequis->getId(),'niveauMin'=>$Prerequis->getNiveauMin(),'equipement'=>$Prerequis->getEquipement(),'niveauMax'=>$Prerequis->getNiveauMax()]);
+      $statement->execute(['id'=>$prerequis->getId(),'niveauMin'=>$prerequis->getNiveauMin(),'equipement'=>$prerequis->getEquipement(),'niveauMax'=>$prerequis->getNiveauMax()]);
       return Database::getInstance()->getConnexion()->lastInsertId();
   }
 
