9d0ed93b70b8bc483e8d63e8f03abdb0b2c2fa10
Leo <leo@PORT-21B-118>
2024-04-15 14:56:29 +0200

    ajout update

--- src/model/Rang.php
+++ src/model/Rang.php
@@ -4,13 +4,13 @@ namespace app\guild\model;
 class Rang{
     private int $_id;
     private string $_intitule;
-    private string $_description;
+    private string $_rang;
     private UtilisateurCollection $_utilisateurs;
     private EvenementCollection $_evenements;
-    public function __construct(int $id = 0, string $description = 'Nouveau Membre', string $intitule = 'noob', UtilisateurCollection $utilisateurs = new UtilisateurCollection(), EvenementCollection $evenements = new EvenementCollection()) {
+    public function __construct(int $id = 0, string $rang = 'Nouveau Membre', string $intitule = 'noob', UtilisateurCollection $utilisateurs = new UtilisateurCollection(), EvenementCollection $evenements = new EvenementCollection()) {
         $this->_id= $id;
         $this->_intitule = $intitule;
-        $this->_description = $description;  
+        $this->_rang = $rang;  
         $this->_utilisateurs = $utilisateurs;
         $this->_evenements = $evenements;
         
@@ -45,17 +45,24 @@ class Rang{
     {
         $this->_intitule= $intitule;
     }
-    public function getDescription():string
+    public function getRang():string
     {
-        return $this->_description;
+        return $this->_rang;
     }
-    public function setDescription(string $description)
+    public function setRang(string $rang)
     {
-        $this->_description= $description;
+        $this->_rang= $rang;
     }
     public static function delete(Rang $id)
     {
         $statement = Database::getInstance()->getConnexion()->prepare('DELETE FROM Rang WHERE id =:id');
         $statement->execute(['id'=>$id->getId()]);
+    }
+
+    public static function update(Rang $rang)
+    {
+        $statement = Database::getInstance()->getConnexion()->prepare
+        ('UPDATE RANG set intitule=:intitule, rang=:rang WHERE id =:id');
+        $statement->execute(['intitule'=>$rang->getIntitule(),'rang'=>$rang->getRang(),'id'=>$rang->getId()]);
     }
 }
