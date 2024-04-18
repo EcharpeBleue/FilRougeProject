66fe4a9a582a68566ef62b072eb35c055a49ffdf
Yang Fu <yang.fu3785@gmail.com>
2024-04-18 09:10:36 +0200

    controller

--- src/controller/ApiController.php
+++ src/controller/ApiController.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace app\guild\controller;
 
-use app\guild\model\Personnage;
-
-class ApiController {
-
-    public static function personnage() {
-        header('Content-Type: application/json; charset=utf-8');
-        $liste = Personnage::list();
-        echo json_encode($liste); 
+class ApiController extends BaseController
+{
+    public function index()
+    {
+        $this->addParam('message',"Salut");
+        $this->view('api/index');
     }
+
 }
---
+++ src/controller/PersonnageController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\guild\controller;
+
+use app\guild\model\Personnage;
+
+class ApiController extends BaseController
+{
+ 
+        public static function personnage() {
+            
+            header('Content-Type: application/json; charset=utf-8');
+            $liste = Personnage::list();
+            echo json_encode($liste); 
+        }
+    
+}
