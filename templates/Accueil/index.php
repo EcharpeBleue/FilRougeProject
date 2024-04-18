a34480e3a4a3834323302882048c48aa666fa558
Leo <leo@PORT-21B-118>
2024-04-18 12:01:58 +0200

    view

--- templates/Accueil/index.php
+++ templates/Accueil/index.php
@@ -1,1 +1,12 @@
-salut
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/Acceuil/style.css">
+</head>
+<body>
+<img src="" alt="./assets/images/01.jfif">
+</body>
+</html>
